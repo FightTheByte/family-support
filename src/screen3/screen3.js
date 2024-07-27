ac510452350acd6f27e50db6de9031f5b5049478
Martin Cameron <mcameron.brix@gmail.com>
2024-07-27 14:23:48 +0100

    add profile pic placeholder for client meeting

--- src/screen3/screen3.js
+++ src/screen3/screen3.js
@@ -1,9 +1,14 @@
 import './screen3.css';
+import profile from '../assets/profile.png';
 
 export function Screen3(){
 
     return(
         <div className="screen3-container">
+            <img 
+                src={profile}
+                className='profile'
+            ></img>
             <p className='description-text'>
                 Caithness Family Support Group was started roughly two and a half years ago. It is a peer led organic group for people who have 
                 a loved one with problematic addiction issues. Living with a loved one who has an addiction is a lonely, chaotic place. Stigma 
