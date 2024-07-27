a2ac9e0465e5ee05be9e2f92d52415b11a9536d0
Martin Cameron <mcameron.brix@gmail.com>
2024-07-27 17:12:23 +0100

    adding meetings component

--- src/screen3/screen3.js
+++ src/screen3/screen3.js
@@ -10,11 +10,11 @@ export function Screen3(){
                 className='profile'
             ></img>
             <p className='description-text'>
-                Caithness Family Support Group was started roughly two and a half years ago. It is a peer led organic group for people who have 
+                Caithness Family Support Group was started roughly two and a half years ago. It is an organic, peer led, group for people who have 
                 a loved one with problematic addiction issues. Living with a loved one who has an addiction is a lonely, chaotic place. Stigma 
                 from other people is both hurtful and judgemental. One persons addiction can affect another eleven people. Talking with others 
-                who are in a similar place is a great relief to know there are others in the same situation. We share experiences of what has been 
-                helpful, where and how to access help and guidance. Also knowing you and your loved ones rights. Come and feel free to say what you 
+                who are in a similar place is a great relief. It's helpful to know that there are others in the same situation. We share experiences of what has been 
+                helpful, where and how to access help and guidance. We also provide information about you and your loved one's rights. Come and feel free to share your experience, say what you 
                 want without being judged. 
             </p>
         </div>
--- src/screen5/screen5.js
+++ src/screen5/screen5.js
@@ -1,6 +1,12 @@
 import './screen5.css';
+import {
+    useEffect, 
+    useState
+} from 'react';
 
 export function Screen5(){
+  
+    const [meetings, setMeetings] = useState();
 
     return(
         <div>
