29a48552189fe46c62f598fe5f4f2cb818322d84
Martin Cameron <mcameron.brix@gmail.com>
2024-08-02 14:08:21 +0100

    added automated meetings population and css styling

--- src/screen5/screen5.js
+++ src/screen5/screen5.js
@@ -7,17 +7,44 @@ import React from 'react';
 
 export const Screen5 = React.forwardRef((props, ref) => {
   
-    const [meetings, setMeetings] = useState();
+    useEffect(() => {
+        //fetch()
+        //.then(res => res.json())
+        //.then(jsonRes => setMeetings(jsonRes))
+    }, [])
+    
+    const [meetings, setMeetings] = useState([
+        {
+            day: 'Monday',
+            time: '7-8pm',
+            place: 'location A'
+        },
+        {
+            day: 'Friday',
+            time: '6-6:30pm',
+            place: 'location B'
+        }
+    ]);
 
     return(
         <div 
             className='screen5-container'
             ref={ref} 
-        >
-            <div className='meeting-grid'>
+        >   
+            <div className='meeting-location'>
                 <h2>Meetings</h2>
-                <p>Mondays </p>
-                <p>Fridays </p>
+                {
+                    meetings
+                    ?meetings.map((meeting, index) => 
+                        <div 
+                            className='meeting-grid'
+                            key={index}
+                        >
+                            <p>{meeting.day} at {meeting.place} from {meeting.time}</p>
+                        </div>
+                    )
+                    :null
+                }
             </div>
         </div>
     )
