eff66e90b55f2c3a00eb7be36d0505a57258398d
Martin Cameron <mcameron.brix@gmail.com>
2024-08-09 20:20:04 +0100

    added correct email

--- src/screen4/screen4.js
+++ src/screen4/screen4.js
@@ -12,7 +12,7 @@ export const Screen4 = React.forwardRef((props, ref) => {
             <div className='contact-details'>
                 <h4 className='contact-us'>Contact Us</h4>
                 <h4 className='details'>Phone Number: <a href="tel:+4407754763950">07754763950</a></h4>
-                <h4 className='details'>Email: <a href="mailto:caithnessfamilysupportgroup@gmail.com">caithnessfamilysupportgroup@gmail.com</a></h4>
+                <h4 className='details'>Email: <a href="mailto:caithnessfamilygroup@gmail.com">caithnessfamilygroup@gmail.com</a></h4>
             </div>
 
             <div className='screen4-right-container'>
